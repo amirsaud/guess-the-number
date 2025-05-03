cb03d1f4809e858f5e21bcc2d0631807162eb009
Amirhosein mehmandost <amirhoseinmhmndst98@gmail.com>
2025-05-04 01:43:32 +0330

    Add files via upload

---
+++ d.js
@@ -0,0 +1,203 @@
+/*let num=25;
+if (num%2==0) {
+    console.log("زوج");
+    
+} else {
+    console.log("فرد");
+    
+}*/
+
+/*let score=85;
+if (score>=90) {
+    console.log("عالی ");
+    
+} else if (score>=70) {
+    console.log("بدک");
+    
+}else{
+    console.log("بد");
+    
+}*/
+/* for (let i = 1; i <4; i++){
+ console.log("چاپ");
+ 
+ }*/
+/*let co=10;
+while (co <15) {
+    console.log("salam");
+    co++;
+}
+*/
+/*for (let i = 1; i < 10; i++){
+    if (i%2===0) {
+        console.log("زوج");
+        
+    } 
+       
+}*/
+/*let num=-3
+
+if (num===0) {
+    console.log("صفر");
+    
+} else if (num>0) {
+    console.log("مثبت");
+    
+} else if (num<0) {
+    console.log("منفی");
+    
+}*/
+/*let deggree=30;
+if (deggree>35) {
+    
+} else {
+    
+}*/
+/*function greet(name) {
+
+    console.log("سلام "+name);
+    
+}
+/*greet("امیر");*/
+/*function bob(a,b) {
+    return a+b
+}
+let result=bob(5,7)
+console.log(result);*/
+/*function sen(chekage) {
+    if (chekage < 13) {
+        console.log("کودک");
+    } else if (chekage < 18) {
+        console.log("جوان");
+    } else {
+        console.log("بزرگسال");
+    }
+}
+
+sen(14);  // جوان
+sen(20);  // بزرگسال
+sen(10);  // کودک
+*/
+/*let count=1;
+while (count<=5) {
+    console.log("ff");
+    count++;
+}*/
+/*function bob(a,b) {
+    if (a>b) {
+        console.log("بزرگتر ");
+        
+    } else if (a<b) {
+        console.log("کوچکتر ");
+        
+    }else{
+        console.log("مساوی");
+        
+    }
+    
+
+
+}
+let hoh=bob(7,7);
+console.log(hoh);*/
+
+/*function calcage(birthage) {
+  return 2025-birthage;
+};
+console.log(calcage(2005));*/
+/*function add(a,b) {
+    if (a>b) {
+return a;        
+    } else {
+return b;        
+    }
+}
+let bon=add(5,8);
+console.log(bon);
+
+typeof 10;
+let a=5;
+let b=6;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/5);
+
+let score=41;
+if (score>50) {
+    console.log("بزرگتر");
+    
+} else {
+    console.log("کوچکتر");
+    
+}
+   
+   let cos=8;
+   if (cos>=18) {
+    console.log("عالی ");
+    
+   } else if (cos>=14) {
+    console.log("متوسط");
+    
+   } else {
+    console.log("بد ");
+    
+   }
+
+let day = "شنبه"
+switch (day) {
+    case "شنبه":
+        console.log('اول');
+
+        break;
+    case"جمعه":
+    console.log("آخر");
+    
+    break;
+
+    default:
+        console.log("معمولی");
+        
+        break;
+}
+
+for (let i = 0; i < 5; i++) {
+console.log("فخری");
+
+}
+
+let i=10;
+while (i>5) {
+    console.log("فخری");
+    --i;
+}
+let b=5;
+do {
+    console.log("سوسن");
+    b++;
+} while (b<=10);
+ زوج وفرد
+ let add=prompt("عدد")
+if (add<=10) {
+    if (add%2==0) {
+        console.log("زوح");
+        
+    } else {
+        console.log("فرد");
+        
+    }
+
+
+} else {
+    console.log("خظا");
+    
+}*/
+let random=Math.floor(Math.random() * 10 )+1;
+let inputnumber=prompt("عدد وارد کن ");
+if (random==inputnumber) {
+    console.log("درست");
+    
+} else {
+    console.log("غلط");
+    
+}
